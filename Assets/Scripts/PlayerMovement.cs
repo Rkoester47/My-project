30acbb9f8ede7dd52223fb5f9fe3f36cbd0c6aea
Rkoester47 <koestr@rpi.edu>
2025-02-03 16:57:21 -0500

    Movement and Menu
    Reworked the movement code, and added a Main Menu scene with a working play button.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,42 +3,23 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
 
     private Rigidbody2D body;
-    private bool grounded;
-    private float horizontal;
-    private float jumpingPower = 16f;
+  
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
     }
 
-    private void FixedUpdate()
-    {
-        body.linearVelocity = new Vector2(horizontal*speed, jumpingPower);
-    }
-
-    private bool isGrounded()
-    {
-        return Physics2D.OverlapCircle(groundCheck.position, .2f, groundLayer);
-    }
 
     private void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && isGrounded())
-        {
-            body.linearVelocity = new Vector2(body.linearVelocity.x * speed, speed);
-        }
-        if (Input.GetButtonUp("Jump") && body.linearVelocity.y > .0f)
-        {
-            body.linearVelocity = new Vector2(body.linearVelocity.x, body.linearVelocity.y * 0.5f);
-        }
-        
+        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
+
+        if (Input.GetKey(KeyCode.Space))
+            body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
 
     }
 }
